52df1baab9bdb838f0e13e67d42d16aec0730699
taemin <taminging@kakao.com>
2021-12-07 23:07:53 +0900

    "tests : users create test"

--- src/test/java/com/taemin/tadadak/user/UsersTest.java
+++ src/test/java/com/taemin/tadadak/user/UsersTest.java
@@ -1,4 +1,16 @@
 package com.taemin.tadadak.user;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.taemin.tadadak.user.domain.Users;
+
 public class UsersTest {
+
+	@Test
+	public void create() {
+		Users users = new Users();
+		assertThat(users.count()).isEqualTo(0);
+	}
 }
